583318326237627ff7aaa0d51970033862953055
danielnoo <dannynoo@gmail.com>
2021-09-16 21:27:38 -0400

    initial pseudo-code

---
+++ index.js
@@ -0,0 +1,26 @@
+// Scope - MVP stuff
+
+// User opens the page to a title header at the top followed by a center console that contains a form. The form contains some dropdowns that shape the type of trivia game they would like to play. They can choose between number of games, category and difficulty. Below the dropdown options menus is a large submit button that begins their game.
+
+// The submitted data is used to create an api call to the trivia API. The response from the API data is stored in a variable such as app.questions. For each question, the data will be display on the page for the user. An h2 heading will display the questions followed by a ul containing 3 wrong answers and 1 right answer in randomized order. When the user clicks on an answer, their answer/score is stored in a variable and the next question in the questions array is immediately painted to the screen.
+
+// This continues until all questions complete. The user is then presented with their score/total.
+// A play again button is presented giving the user the option to play again.
+
+
+
+
+
+// STRETCH
+
+
+// Implement site tokens to improve reusability. - maybe localstorage?
+
+// A timer that starts at the beginning of each question.
+// A score that is calculated based on the correctness of answer and time remaining.
+
+// More complex play again button at the end of the game.
+
+// Social sharing option to challenge friends to beat their score
+
+// Add sound effects/music
