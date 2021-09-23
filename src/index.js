369d6dd9e4bafcd8d50bbbffab9db50f8640d558
Ryan May <ryan777may@gmail.com>
2021-09-23 19:01:42 -0400

    Fixed issue with timer on last question, added final score/question total

--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
-
-
-
-
-
-
-
-
-
-
 // DESCRIPTION OF PROJECT
 // A TRIVIA GAME POWERED BY THE OPEN TRIVIA API
 
@@ -32,6 +22,7 @@ app.timer;
 app.questionNumber = 0;
 app.questions = [];
 app.firstRun = true;
+app.numOfCorrect = 0;
 
 app.welcomeScreen = document.querySelector('.welcome');
 app.gameScreen = document.querySelector('.game');
@@ -44,14 +35,14 @@ app.questionCounterDisplay = document.querySelector('#questionsCounter');
 // Add event listener for landing page form to get game options from user 
 app.getUserOptions = () => {
     const form = document.querySelector('form');
-    form.addEventListener('submit', function(e) {
+    form.addEventListener('submit', function (e) {
         e.preventDefault();
         const category = document.querySelector('#category').value;
         const difficulty = document.querySelector('#difficulty').value;
-        const numOfQuestions = document.querySelector('#numberOfQuestions').value;
+        app.numOfQuestions = document.querySelector('#numberOfQuestions').value;
         app.welcomeScreen.style.display = 'none';
         app.gameScreen.style.display = 'block';
-        app.apiCall(category, difficulty, numOfQuestions);
+        app.apiCall(category, difficulty, app.numOfQuestions);
     })
 }
 
@@ -69,25 +60,24 @@ app.apiCall = (categoryOption, difficultyOption, numOfQuestions) => {
         difficulty: difficultyOption,
         type: 'multiple'
     });
-    
+
     fetch(apiUrl)
-    .then(response => response.json())
-    .then(res => {
-        console.log(res)
-        app.questions = res.results.map(question => {
-            question.incorrect_answers.push(question.correct_answer);
-            return {
-                wrongAnswers: question.incorrect_answers,
-                correctAnswer: question.correct_answer,
-                question: question.question
-            };
-    });
-})
-    
-    .then(() => {
-        app.intervalID = setInterval(app.timerInterval, 1000);
-        app.intervalID;
-    });
+        .then(response => response.json())
+        .then(res => {
+            app.questions = res.results.map(question => {
+                question.incorrect_answers.push(question.correct_answer);
+                return {
+                    wrongAnswers: question.incorrect_answers,
+                    correctAnswer: question.correct_answer,
+                    question: question.question
+                };
+            })
+        })
+        .then(() => {
+
+            app.intervalID = setInterval(app.timerInterval, 1000);
+            app.intervalID;
+        })
 }
 
 //create display function. Create new element on page for storing questions and answers.
@@ -97,6 +87,7 @@ app.apiCall = (categoryOption, difficultyOption, numOfQuestions) => {
 //append both elements to the required section
 
 app.displayResults = () => {
+    console.log(app.questionNumber)
     app.firstRun = false;
     app.gameSection.innerHTML = '';
     app.newQuestion = document.createElement('h2')
@@ -105,14 +96,14 @@ app.displayResults = () => {
     app.currentQuestion.wrongAnswers.sort();
 
     app.newQuestion.innerHTML = app.currentQuestion.question;
-    
-    app.currentQuestion.wrongAnswers.forEach( (answer, index) => {
+
+    app.currentQuestion.wrongAnswers.forEach((answer, index) => {
         const answerHTML = document.createElement('li');
         answerHTML.innerHTML = `<p>${answer}</p>`;
-        answerHTML.classList.add('answer',`answer${index}`);
+        answerHTML.classList.add('answer', `answer${index}`);
         app.newAnswers.append(answerHTML)
     })
-    app.gameSection.append(app.newQuestion,app.newAnswers);
+    app.gameSection.append(app.newQuestion, app.newAnswers);
     app.questionCounterDisplay.textContent = `Question number: ${app.questionNumber + 1}`;
     app.checkAnswer();
 
@@ -120,17 +111,18 @@ app.displayResults = () => {
 }
 // a function that adds event listeners to the answer elements after they are painted to the page -- if clicked, app.score is incremented else not
 app.checkAnswer = () => {
-    app.newAnswers.addEventListener('click', function(e) {
+    app.newAnswers.addEventListener('click', function (e) {
         app.userAnswer = e.target.textContent;
         const isCorrect = document.createElement('p');
-        
+
         if (app.userAnswer === app.currentQuestion.correctAnswer) {
             app.score += 100 + ((app.timer * app.timer) / 2);
             isCorrect.textContent = 'correct';
             e.target.append(isCorrect);
+            app.numOfCorrect++;
             app.questionNumber++;
             app.scoreDisplay.textContent = `Score: ${app.score}`;
-        }else {
+        } else {
             isCorrect.textContent = 'wrong';
             e.target.append(isCorrect);
             app.questionNumber++;
@@ -143,19 +135,24 @@ app.checkAnswer = () => {
 
 
 app.timerInterval = () => {
-//If questions remain and timer has reached zero or first run is true, then run display function, reset timer to original start and increase question number. If questions remaining is 0 and timer has reached 0, present user with final score and ask if they want to play again.
- if (app.questionNumber === app.questions.length && app.userAnswer) {
-     //game over
-        clearInterval (app.intervalID);
-        app.userAnswer = '';
-    // present play again button on a modal 
+    //If questions remain and timer has reached zero or first run is true, then run display function, reset timer to original start and increase question number. If questions remaining is 0 and timer has reached 0, present user with final score and ask if they want to play again.
+    if (app.questionNumber === app.questions.length  && app.userAnswer || app.questionNumber === app.questions.length -1 && app.timer === 0) {
+        //game over
+        console.log('game over')
+        clearInterval(app.intervalID);
+        // present play again button on a modal 
+        const endScreen = document.querySelector('#endScreenText');
+        endScreen.innerHTML = `
+            <p>${app.numOfCorrect}/${app.numOfQuestions} questions correct.</p>
+            <p>Final Score: ${app.score}</p>`
         document.querySelector('.endGameModal').classList.remove('hidden');
-    } else if (app.timer === 0 && app.questionNumber < app.questions.length) { //Checks if timer has run out without an answer being given and advances to next question
+    } else if (app.timer === 0 && app.questionNumber < app.questions.length - 1) { //Checks if timer has run out without an answer being given and advances to next question
+        console.log('this ran')
         app.userAnswer = '';
         app.questionNumber++;
         app.displayResults();
         app.timer = 30;
-    }  else if (app.firstRun === true || app.userAnswer) {
+    } else if (app.firstRun === true || app.userAnswer) {
         //display next question and reset timer
         setTimeout(() => {
             app.userAnswer = '';
@@ -163,7 +160,6 @@ app.timerInterval = () => {
         }, 1000);
         app.timer = 30;
     }
-    app.timerDisplay.textContent = '';
     app.timerDisplay.textContent = `Time Left: ${app.timer--}`;
 }
 
@@ -172,13 +168,13 @@ app.init = () => {
 }
 
 app.init();
- 
 
 
 
 
 
- 
+
+
 
 
 
